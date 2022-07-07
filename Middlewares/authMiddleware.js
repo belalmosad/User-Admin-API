464ddf55ed1f3836bed57ce5fd1263b6f90fa336
unknown <belalelmeligy516@gmail.com>
2022-07-07 16:53:45 +0200

    assigned decodedToken.isAdmin to req.isAdmin to be used in user Controller (to check if the token carrier is an admin)

--- Middlewares/authMiddleware.js
+++ Middlewares/authMiddleware.js
@@ -5,6 +5,7 @@ module.exports = (req, res, next) => {
         let encodedToken = req.get('Authorization').split(' ')[1];
         let decodedToken = jwt.verify(encodedToken, process.env.SECRET_KEY);
         req.id = decodedToken.id;
+        req.isAdmin = decodedToken.isAdmin;
         next();
     } catch (error) {
         error.message = 'You are noth authorized to show this data';
