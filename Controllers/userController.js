1f8e297f2def791e7c227c37aeed7e3d76259733
unknown <belalelmeligy516@gmail.com>
2022-07-07 03:41:31 +0200

    Implemented Add user functionality

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,9 +1,14 @@
 const User = require('../Models/User');
 
-
 module.exports.getAllUsers = (req, res, next) => {
     User.find()
         .then((data) => res.status(200).json(data))
         .catch((error) => { next(error) })
 }
 
+module.exports.addUser = (req, res, next) => {
+    let newUser = new User(req.body);
+    newUser.save()
+    .then(() => {res.status(201).json({message: 'New user added'})})
+    .catch((err) => {next(err)})
+}
