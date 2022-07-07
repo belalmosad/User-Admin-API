8271165ba706a789dfa18465ec4751922d8c0854
unknown <belalelmeligy516@gmail.com>
2022-07-07 04:34:45 +0200

    Implemented hashing and salting pawwords using bcrypt

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require('../Models/User');
+const bcrypt = require('bcrypt');
 
 module.exports.getAllUsers = (req, res, next) => {
     User.find()
@@ -7,44 +8,51 @@ module.exports.getAllUsers = (req, res, next) => {
 }
 
 module.exports.addUser = (req, res, next) => {
-    let newUser = new User(req.body);
-    newUser.save()
-    .then(() => {res.status(201).json({message: 'New user added'})})
-    .catch((err) => {next(err)})
+
+    bcrypt.hash(req.body.password, 10)
+        .then((value) => {
+            req.body.password = value;
+            let newUser = new User(req.body);
+            newUser.save()
+                .then(() => { res.status(201).json({ message: 'New user added' }) })
+                .catch((err) => { next(err) })
+        })
+        .catch((err) => { next(err) })
+
 }
 
 module.exports.getUserById = (req, res, next) => {
-    User.findOne({id: req.params.id})
-    .then((data) => {
-        if(!data) {
-            throw new Error('User Not Found')
-        } else {
-            res.status(200).json(data)
-        }
-    })
-    .catch ((error) => next(error))
+    User.findOne({ id: req.params.id })
+        .then((data) => {
+            if (!data) {
+                throw new Error('User Not Found')
+            } else {
+                res.status(200).json(data)
+            }
+        })
+        .catch((error) => next(error))
 }
 
 module.exports.updateUser = (req, res, next) => {
-    User.updateOne({id: req.params.id}, {$set: req.body})
-    .then((data) => {
-        if(data.matchedCount == 0) {
-            throw new Error('User Not Found')
-        } else {
-            res.status(200).json({message: 'User Updated'})
-        }
-    })
-    .catch((error) => {next(error)})
+    User.updateOne({ id: req.params.id }, { $set: req.body })
+        .then((data) => {
+            if (data.matchedCount == 0) {
+                throw new Error('User Not Found')
+            } else {
+                res.status(200).json({ message: 'User Updated' })
+            }
+        })
+        .catch((error) => { next(error) })
 }
 
 module.exports.deleteUser = (req, res, next) => {
-    User.deleteOne({id: req.params.id})
-    .then((data) => {
-        if(data.deletedCount == 0) {
-            throw new Error('User Not Found')
-        } else {
-            res.status(200).json({message: 'User Deleted'})
-        }
-    })
-    .catch((error) => next(error));
+    User.deleteOne({ id: req.params.id })
+        .then((data) => {
+            if (data.deletedCount == 0) {
+                throw new Error('User Not Found')
+            } else {
+                res.status(200).json({ message: 'User Deleted' })
+            }
+        })
+        .catch((error) => next(error));
 }
