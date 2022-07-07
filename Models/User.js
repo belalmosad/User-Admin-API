686e5400577ff27bb5acd3e930de27e3523e8caa
unknown <belalelmeligy516@gmail.com>
2022-07-07 03:42:29 +0200

    Added new Property in Schema: unique Id

--- Models/User.js
+++ Models/User.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 
 const schema = new mongoose.Schema({
+    id: {
+        type: Number,
+        required: true,
+        uniqie: true
+    },
     name: {
         type: String,
         required: true
