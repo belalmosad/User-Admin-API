13282f9343bb39b8c168ff1bdf52e9c431206640
unknown <belalelmeligy516@gmail.com>
2022-07-07 03:43:19 +0200

    Added new route for post method

--- Routes/userRoute.js
+++ Routes/userRoute.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const userRouter = express.Router();
 const userController = require('../Controllers/userController');
+const validationMiddleware = require('../Middlewares/validationMiddleware');
+const {body} = require('express-validator');
 
 userRouter.route('/users')
     .get(userController.getAllUsers)
+    .post([
+        body('email').isEmail().withMessage('Incorrect Email Format'),
+        body('password').isStrongPassword().withMessage('You should Enter Strong Password')
+    ], validationMiddleware, userController.addUser)
 
 
 module.exports=userRouter;
